02dadec9b40bb846eb1b643f5898af9b831424e4
anmedina-arg <andres.medina.arg@gmail.com>
2023-03-03 16:22:20 -0300

    change bg color

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -29,7 +29,7 @@ function Contact() {
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
       <div className="container contact__container">
-        {/* <div className="contact__options">
+        <div className="contact__options">
           <article className="contact__option">
             <h4>Email</h4>
             <h5>andres.medina.arg@gmail.com</h5>
@@ -40,11 +40,11 @@ function Contact() {
           <article className="contact__option">
             <h4>WhatsApp</h4>
             <h5>+5493816713512</h5>
-            <a href="https://wa.me/5493816713512" target="_blank">
+            <a href="https://wa.me/5493816713512?text=Hola,%20¿cómo%20estás?" target="_blank">
               Send a message
             </a>
           </article>
-        </div> */}
+        </div>
         {/* END OF OPTIONS */}
         <form ref={form} onSubmit={sendEmail}>
           <input
