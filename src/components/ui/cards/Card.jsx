2d8b73a18080a71b08b0a3ffae9278c3ff2227fd
anmedina-arg <andres.medina.arg@gmail.com>
2023-07-01 11:50:09 -0300

    cleaning cards

--- src/components/ui/cards/Card.jsx
+++ src/components/ui/cards/Card.jsx
@@ -1,46 +1,36 @@
-import styles from './card.module.css';
+import styles from "./card.module.css";
 
 const Card = ({ id, image, title, github, demo, description, skillArr }) => {
   return (
     <>
       <article className={styles.portfolio__item} key={id}>
         <div className={styles.adentro}>
-          <img className={styles.card__image} src={ image } alt='imagen de proyecto' />
-          <h3>{ title }</h3>
-          <p>{ description}</p>
+          <img
+            className={styles.card__image}
+            src={image}
+            alt="imagen de proyecto"
+          />
+          <h3>{title}</h3>
+          {/* <p>{ description}</p> */}
           <ul>
-            {
-              skillArr?.map((e, index) => {
-                return(
-                  <li key={index}>{ e }</li>
-                )
-              })
-            }
-            </ul>
+            {skillArr?.map((e, index) => {
+              return <li key={index}>{e}</li>;
+            })}
+          </ul>
           <div className={styles.portfolio__item_cta}>
-            <a
-              href={github}
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-            >
+            <a href={github} className="btn" target="_blank" rel="noreferrer">
               Github
             </a>
           </div>
           <div>
-            <a
-            href={demo}
-            className="btn"
-            target="_blank"
-            rel="noreferrer"
-            >
+            <a href={demo} className="btn" target="_blank" rel="noreferrer">
               Demo
             </a>
           </div>
         </div>
       </article>
     </>
-  )
+  );
 };
 
 export default Card;
