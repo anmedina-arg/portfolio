32af17b4434a2b167c11603c51046f8bb855d70b
anmedina-arg <andres.medina.arg@gmail.com>
2023-07-20 14:09:41 -0300

    some little changes on cards and add mock data

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,22 +1,25 @@
 import styles from "./card.module.css";
 
-const Card = ({ id, image, title, github, demo, description, skillArr }) => {
+const Card = ({ project, skillArr }) => {
+  const { id, image, title, github, demo } = project; //falta description
   return (
     <>
-      <article className={styles.portfolio__item} key={id}>
-        <div className={styles.adentro}>
-          <img
-            className={styles.card__image}
-            src={image}
-            alt="imagen de proyecto"
-          />
-          <h3>{title}</h3>
-          {/* <p>{ description}</p> */}
-          <ul>
-            {skillArr?.map((e, index) => {
-              return <li key={index}>{e}</li>;
-            })}
-          </ul>
+      <article className={styles.card__container} key={id}>
+        <div className={styles.interior}>
+          <div className={styles.content}>
+            <img
+              className={styles.card__image}
+              src={image}
+              alt="imagen de proyecto"
+            />
+            <h3>{title}</h3>
+            {/* <p>{ description}</p> */}
+            <ul>
+              {skillArr?.map((e, index) => {
+                return <li key={index}>{e}</li>;
+              })}
+            </ul>
+          </div>
           <div className={styles.portfolio__item_cta}>
             <a href={github} className="btn" target="_blank" rel="noreferrer">
               Github
